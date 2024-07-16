9ed22446432d5e4232c6a6803f9d211a982cd785
unknown <kad2321@yahoo.com>
2024-07-16 14:56:18 -0400

    checkFive Done.

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,10 +1,19 @@
-const test = require('../checkFive.js')
+const checkFive = require('../checkFive.js')
 
 describe("checkFive", function() {
     
-    test("should produce 'is less than five' for number < 5", function() {
+    test("should produce 'is less than 5.' for number < 5", function() {
         let output = checkFive(2);
-        expect(output).toEqual("2 is less than 5");
-    });
+        expect(output).toEqual("2 is less than 5.");
+    })
+
+    test("should produce 'is equal to 5.' for number = 5", function() { 
+        let output = checkFive(5);
+        expect(output).toEqual('5 is equal to 5.');
+    })
     
+    test("should produce 'is greater than 5.' for number > 5", function() {
+        let output = checkFive(6);
+        expect(output).toEqual('6 is greater than 5.');
+    })
 });
